220322f1528022e8c3afae6e26cfce83afcf6356
mainamuriuki274 <muriukimainalewis@gmail.com>
2021-03-17 19:04:40 +0300

    React Tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+
 
 function App() {
+
+  const name = 'Lewis Maina Muriuki'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title = "My Task Tracker" />
+      <h1>Hello from React</h1>
+      <h2>Hello { name }</h2>
     </div>
   );
 }
 
 export default App;
+   
